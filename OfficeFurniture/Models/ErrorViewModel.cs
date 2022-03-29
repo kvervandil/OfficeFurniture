8ffce4b0298c039937923331198b83260f04db7f
Jakub Gliński <jglinski@pgs-soft.com>
2022-03-29 15:16:22 +0200

    Add project files.

---
+++ OfficeFurniture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OfficeFurniture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
